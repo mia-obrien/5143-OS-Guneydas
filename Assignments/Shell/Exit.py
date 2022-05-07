1a0b3d2c8e2da6b7f657239f3a2a085dc87bb50b
Mihri <mihribanguneydas@gmail.com>
2022-05-07 09:11:42 -0500

    Create Exit.py

---
+++ Assignments/Shell/Exit.py
@@ -0,0 +1,14 @@
+#import sys
+
+#def exit(**kwargs):
+#    sys.stdout.write('Exiting Shell!')
+#    sys.stdout.flush()
+#    sys.exit()
+#    sys.stdout.write('Exiting Shell!')
+
+
+import sys
+
+def exit(**kwargs):
+    print('\nExiting Shell!')
+    sys.exit()
